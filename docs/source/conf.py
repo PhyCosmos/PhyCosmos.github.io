a47c63dcb34be08bfcc033415b0dae25b21233a7
whitedolphin <howsitgoing@naver.com>
2022-09-30 04:01:12 +0900

    nav-top tomato

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,3 +67,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_logo = './_static/mylogo.png'
+html_theme_options = {
+     'style_nav_header_background': 'tomato',
+}
